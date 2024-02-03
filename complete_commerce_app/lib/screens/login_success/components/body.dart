3b3d7c1f31b5b3c40d8d1650942e697ba916b62f
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-02-03 16:24:12 +0200

    Success Screen(Image -Success message -home button)

--- complete_commerce_app/lib/screens/login_success/components/body.dart
+++ complete_commerce_app/lib/screens/login_success/components/body.dart
@@ -1,14 +1,38 @@
+import 'package:complete_commerce_app/components/default_button.dart';
 import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: [
-        
-      ]),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(height: SizeConfig.screenHeight * .04),
+        Image.asset(
+          "assets/images/success.png",
+          height: SizeConfig.screenHeight * .4,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+        SizedBox(height: SizeConfig.screenHeight * .08),
+        Text(
+          "Login Success",
+          style: Theme.of(context).textTheme.displaySmall!.copyWith(
+              fontWeight: FontWeight.bold,
+              fontSize: getProportionateScreenWidth(30)),
+        ),
+        const Spacer(),
+        SizedBox(
+          width: SizeConfig.screenWidth * .6,
+          child: DefaultButton(text: "Back to home", press: () {}),
+        ),
+        const Spacer(),
+      ],
     );
   }
 }
