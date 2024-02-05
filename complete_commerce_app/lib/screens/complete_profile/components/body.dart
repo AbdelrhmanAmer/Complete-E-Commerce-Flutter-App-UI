027382e307ca83c6baa65f27d05fdc0a299ccab6
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-02-05 14:50:05 +0200

    Complete Profile (Last Touch)

--- complete_commerce_app/lib/screens/complete_profile/components/body.dart
+++ complete_commerce_app/lib/screens/complete_profile/components/body.dart
@@ -14,19 +14,33 @@ class Body extends StatelessWidget {
       width: double.infinity,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-        child: Column(children: [
-          SizedBox(
-              height:
-                  getProportionateScreenHeight(SizeConfig.screenHeight * .03)),
-          const PageTitleAndSubtitle(
-            title: "Complete Profile",
-            subtitle: "Complete your details or continue\nwith your social media",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                  height: getProportionateScreenHeight(
+                      SizeConfig.screenHeight * .02)),
+              const PageTitleAndSubtitle(
+                title: "Complete Profile",
+                subtitle:
+                    "Complete your details or continue\nwith your social media",
+              ),
+              SizedBox(
+                height:
+                    getProportionateScreenHeight(SizeConfig.screenHeight * .05),
+              ),
+              CompleteProfileFrom(),
+              SizedBox(
+                  height: getProportionateScreenHeight(kDefaultPadding)),
+              Text(
+                "By continuing your confirm that you agree\nwith our Term and Condition.",
+                style: TextStyle(fontSize: getProportionateScreenWidth(12)),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: getProportionateScreenHeight(kDefaultPadding)),
+            ],
           ),
-          SizedBox(
-            height: getProportionateScreenHeight(SizeConfig.screenHeight * .08),
-          ),
-          CompleteProfileFrom(),
-        ]),
+        ),
       ),
     );
   }
--- complete_commerce_app/lib/screens/complete_profile/components/complete_profile_form.dart
+++ complete_commerce_app/lib/screens/complete_profile/components/complete_profile_form.dart
@@ -1,5 +1,6 @@
 import 'package:complete_commerce_app/components/custom_suffix_icon.dart';
 import 'package:complete_commerce_app/components/default_button.dart';
+import 'package:complete_commerce_app/constants.dart';
 import 'package:complete_commerce_app/size_config.dart';
 import 'package:flutter/material.dart';
 
@@ -8,49 +9,91 @@ class CompleteProfileFrom extends StatelessWidget {
   CompleteProfileFrom({super.key});
 
   String fName = "", lName = "", phone = "", address = "";
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(children: [
         TextFormField(
+          initialValue: fName == "" ? null : fName,
           decoration: const InputDecoration(
               label: Text("First Name"),
               hintText: "Enter your first name",
               suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/User.svg")),
           keyboardType: TextInputType.text,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           onSaved: (newValue) => fName = newValue!,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return kNameNullError;
+            }
+            return null;
+          },
         ),
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
+        SizedBox(height: SizeConfig.screenHeight * 0.03),
         TextFormField(
+          initialValue: lName == "" ? null : lName,
           decoration: const InputDecoration(
               label: Text("Last Name"),
               hintText: "Enter your Last name",
               suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/User.svg")),
           keyboardType: TextInputType.text,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           onSaved: (newValue) => lName = newValue!,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return kNameNullError;
+            }
+            return null;
+          },
         ),
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
+        SizedBox(height: SizeConfig.screenHeight * 0.03),
         TextFormField(
+          initialValue: phone == "" ? null : phone,
           decoration: const InputDecoration(
               label: Text("Phone Number"),
               hintText: "Enter your phone number",
               suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/Phone.svg")),
           keyboardType: TextInputType.number,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           onSaved: (newValue) => phone = newValue!,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return kPhoneNumberNullError;
+            } else if (value.length != 11) {
+              return kShortPhoneNumberError;
+            }
+            return null;
+          },
         ),
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
+        SizedBox(height: SizeConfig.screenHeight * 0.03),
         TextFormField(
+          initialValue: address == "" ? null : address,
           decoration: const InputDecoration(
               label: Text("Address"),
               hintText: "Enter your address",
-              suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/Location point.svg")),
+              suffixIcon:
+                  CustomSuffixIcon(svgIcon: "assets/icons/Location point.svg")),
           keyboardType: TextInputType.text,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           onSaved: (newValue) => address = newValue!,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return kAddressNullError;
+            }
+            return null;
+          },
         ),
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
-        DefaultButton(text: "Continue", press: (){}),
-        
+        SizedBox(height: SizeConfig.screenHeight * 0.05),
+        DefaultButton(
+            text: "Continue",
+            press: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+              }
+            }),
       ]),
     );
   }
