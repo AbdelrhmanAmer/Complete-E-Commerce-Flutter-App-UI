7d4ac890e1d40ea2bbb334ecfd29b0569de4dc61
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-02-12 00:26:53 +0200

    Home (Discount Banner background bubbles )

--- complete_commerce_app/lib/screens/home/coponenets/discount_banner.dart
+++ complete_commerce_app/lib/screens/home/coponenets/discount_banner.dart
@@ -10,31 +10,60 @@ class DiscountBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(
-        vertical: kDefaultPadding,
-        horizontal: kDefaultPadding,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFF4A3298),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: RichText(
-        text: TextSpan(
-          text: "A Summer Surprise\n",
-          style: TextStyle(fontSize: getProportionateScreenWidth(11)),
-          children: [
-            TextSpan(
-              text: "Cashback 20%",
-              style: TextStyle(
-                fontSize: getProportionateScreenWidth(24),
-                fontWeight: FontWeight.bold,
-              ),
-            )
-          ],
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(
+            vertical: kDefaultPadding,
+            horizontal: kDefaultPadding,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              const Color(0xFF4A3298).withOpacity(.8),
+              const Color(0xFF4A3298),
+            ]),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: RichText(
+            text: TextSpan(
+              text: "A Summer Surprise\n",
+              style: TextStyle(fontSize: getProportionateScreenWidth(11)),
+              children: [
+                TextSpan(
+                  text: "Cashback 20%",
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(24),
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          right: -20,
+          top: -75,
+          child: Container(
+            height: getProportionateScreenHeight(150),
+            width: getProportionateScreenWidth(150),
+            decoration: BoxDecoration(
+                color: kSecondaryColor.withOpacity(.1),
+                borderRadius: BorderRadius.circular(150)),
+          ),
+        ),
+        Positioned(
+          bottom: -100,
+          left: 10,
+          child: Container(
+            height: getProportionateScreenHeight(150),
+            width: getProportionateScreenWidth(150),
+            decoration: BoxDecoration(
+                color: kSecondaryColor.withOpacity(.1),
+                borderRadius: BorderRadius.circular(150)),
+          ),
+        ),
+      ],
     );
   }
 }
