1889876755d954f88a9613d07fbd84b856ae0c0e
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-02-21 07:52:18 +0200

    Details (counter display)

--- complete_commerce_app/lib/screens/details/components/counter.dart
+++ complete_commerce_app/lib/screens/details/components/counter.dart
@@ -4,20 +4,45 @@ import '../../../constants.dart';
 import '../../../size_config.dart';
 import 'rounded_icon_btn.dart';
 
-class Counter extends StatelessWidget {
+class Counter extends StatefulWidget {
   const Counter({
     super.key,
   });
 
   @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int counter = 1;
+  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         RoundedIconBtn(
-            iconData: Icons.remove, press: () {}, elevation: 2),
-        SizedBox(
-            width: getProportionateScreenHeight(kDefaultPadding*2)),
-        RoundedIconBtn(iconData: Icons.add, press: () {}, elevation: 2)
+            iconData: Icons.remove,
+            press: () {
+              setState(() {
+                if (counter > 1) {
+                  counter--;
+                }
+              });
+            },
+            elevation: 2),
+        SizedBox(width: getProportionateScreenHeight(kDefaultPadding)),
+        Text(
+          "$counter",
+          style: const TextStyle(fontSize: 17),
+        ),
+        SizedBox(width: getProportionateScreenHeight(kDefaultPadding)),
+        RoundedIconBtn(
+            iconData: Icons.add,
+            press: () {
+              setState(() {
+                counter++;
+              });
+            },
+            elevation: 2)
       ],
     );
   }
